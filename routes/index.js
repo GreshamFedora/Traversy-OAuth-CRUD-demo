dc1303b55a7c2c3dfed349e305662596636035e0
Gresham Fedora <gresham.fedora@gmail.com>
2022-09-14 16:18:26 -0400

    updated story

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-
+const Story = require('../models/Story')
 
 
 //@desc Login/landing page
 //@route GET /
-router.get('/', ensureGuest, (req,res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login',
     })
@@ -15,12 +15,16 @@ router.get('/', ensureGuest, (req,res) => {
 
 //@desc Dashboard
 //@route GET /dashboard
-router.get('/dashboard', ensureAuth, (req,res) => {
-    console.log(req.user)
-    res.render('dashboard', {
-        name: req.user.firstName,
-
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 
